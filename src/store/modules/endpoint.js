82d8f60a279eacc71b9aea54a316014f1d5d9f2c
Etienne Bömcke <ebomcke@gmail.com>
2019-01-25 00:13:01 +0100

    feat(): add endpoint creation

--- src/store/modules/endpoint.js
+++ src/store/modules/endpoint.js
@@ -1,11 +1,22 @@
 import createEasyFirestore from "vuex-easy-firestore";
+import firebase from "firebase/app";
+import "firebase/firestore";
 
 const endpoint = {
   firestorePath: "endpoints",
   firestoreRefType: "collection",
   moduleName: "endpoint",
   statePropName: "data",
-  namespaced: true
+  namespaced: true,
+  sync: {
+    defaultValues: {
+      defaultResponse: {
+        status: 200,
+        body: JSON.stringify({ status: "OK" })
+      },
+      lastActive: firebase.firestore.Timestamp.now()
+    }
+  }
 };
 
 export default createEasyFirestore(endpoint, { logging: true });
--- src/views/Endpoints.vue
+++ src/views/Endpoints.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <el-button type="primary" class="new-resource">New endpoint</el-button>
+    <el-button type="primary" class="new-resource" @click="newEndpoint">New endpoint</el-button>
     <el-row>
       <el-col v-for="endpoint in endpoints" :key="endpoint.id" :span="12">
         <EndpointCard v-bind="endpoint"/>
@@ -11,11 +11,26 @@
 
 <script>
 import EndpointCard from "../components/EndpointCard";
+import firebase from "firebase/app";
+import "firebase/firestore";
+
 export default {
   data() {
     return { endpoints: this.$store.state.endpoint.data };
   },
-  components: { EndpointCard }
+  components: { EndpointCard },
+  methods: {
+    newEndpoint() {
+      this.$store
+        .dispatch("endpoint/insert", {
+          account: firebase
+            .firestore()
+            .doc(`/accounts/${this.$store.state.account.account.id}`),
+          path: "my-new-url"
+        })
+        .then(console.log);
+    }
+  }
 };
 </script>
 
