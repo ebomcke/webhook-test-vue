8f1fc6607feea5fb3dfd7ffb60c6202f2eb0b6e2
Etienne Bömcke <ebomcke@gmail.com>
2019-01-25 12:10:34 +0100

    feat(): add webhook search

--- src/store/modules/webhook.js
+++ src/store/modules/webhook.js
@@ -5,7 +5,36 @@ const webhook = {
   firestoreRefType: "collection",
   moduleName: "webhook",
   statePropName: "data",
-  namespaced: true
+  namespaced: true,
+  state: {
+    filters: []
+  },
+  getters: {
+    filteredWebhooks: state => {
+      const pathFilters = state.filters.filter(f => f.startsWith("p-"));
+      const contentFilters = state.filters.filter(f => !f.startsWith("p-"));
+      return Object.values(state.data).filter(
+        webhook =>
+          (pathFilters.length === 0 ||
+            pathFilters.some(f => webhook.path === f.replace("p-", ""))) &&
+          (contentFilters.length === 0 ||
+            contentFilters.every(f => webhook.body.includes(f)))
+      );
+    }
+  },
+  mutations: {
+    setFilters(state, payload) {
+      state.filters = payload.filters;
+    }
+  },
+  actions: {
+    filter({ commit }, payload) {
+      commit({
+        type: "setFilters",
+        filters: payload.filters
+      });
+    }
+  }
 };
 
 export default createEasyFirestore(webhook, { logging: true });
--- src/views/Webhooks.vue
+++ src/views/Webhooks.vue
@@ -1,5 +1,23 @@
 <template>
   <div>
+    <div class="webhook-search">
+      <el-select
+        v-model="filters"
+        multiple
+        filterable
+        allow-create
+        default-first-option
+        placeholder="Search by path or content"
+        @change="updateFilters"
+      >
+        <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        ></el-option>
+      </el-select>
+    </div>
     <el-row>
       <el-col v-for="webhook in webhooks" :key="webhook.id" :span="12">
         <WebhookCard v-bind="webhook"/>
@@ -13,8 +31,39 @@ import WebhookCard from "../components/WebhookCard";
 
 export default {
   data() {
-    return { webhooks: this.$store.state.webhook.data };
+    const pathOptions = Object.values(this.$store.state.endpoint.data).map(
+      endpoint => ({
+        label: `path: /${endpoint.path}`,
+        value: `p-${endpoint.path}`
+      })
+    );
+    return {
+      filters: this.$store.state.webhook.filters,
+      options: pathOptions
+    };
   },
-  components: { WebhookCard }
+  computed: {
+    webhooks() {
+      return this.$store.getters["webhook/filteredWebhooks"];
+    }
+  },
+  components: { WebhookCard },
+  methods: {
+    updateFilters(filters) {
+      this.$store.dispatch({
+        type: "webhook/filter",
+        filters
+      });
+    }
+  }
 };
 </script>
+
+<style scoped>
+.webhook-search {
+  margin: 11px 0;
+}
+.webhook-search > .el-select {
+  display: block;
+}
+</style>
